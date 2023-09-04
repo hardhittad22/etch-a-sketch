1fa359c2dc242cdeadc25655b46902dbecdfb368
hardhittad22 <133292594+hardhittad22@users.noreply.github.com>
2023-09-04 18:12:14 -0400

    working etch with black button

--- script.js
+++ script.js
@@ -1,25 +1,88 @@
-const container = document.querySelector('.container');
-const resetButton = document.getElementById('resetButton');
+let color = "black";
+let click = false;
 
-function createGrid(size) {
-  container.innerHTML = ''; // Clear the current grid
-  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+document.addEventListener("DOMContentLoaded", function () {
+  const board = document.querySelector(".board");
 
-  for (let i = 0; i < size * size; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+  // Initial board size
+  createBoard(32);
+
+  // Toggle drawing mode on body click
+  document.querySelector("body").addEventListener("click", function(e) {
+    if (e.target.tagName != "BUTTON") {
+      click = !click;
+      let draw = document.querySelector("#draw");
+      if(click) {
+        draw.textContent = "Now you can draw";
+      } else {
+        draw.textContent = "You're not allowed to draw";
+      }
+    }
+  });
+
+  // Reset board button
+  let btn_reset = document.querySelector("#reset");
+  btn_reset.addEventListener("click", function () {
+    resetBoard();
+  });
+
+  // Change color button
+  let btn_changeColor = document.querySelector("#changeColor");
+  btn_changeColor.addEventListener("click", function () {
+    changeColor();
+  });
+});
+
+function createBoard(size) {
+  const board = document.querySelector(".board");
+
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let numDivs = size * size;
+
+  for (let i = 0; i < numDivs; i++) {
+    let div = document.createElement("div");
+    div.addEventListener("mouseover", colorDiv);
+    board.insertAdjacentElement("beforeend", div);
   }
 }
 
-resetButton.addEventListener('click', () => {
-  const newSize = parseInt(prompt('Enter the number of squares per side (max 100):'), 10) || 16;
-  if (newSize > 100) {
-    alert('Please enter a value up to 100.');
-    return;
+function getSize() {
+  let input = prompt("What will be the size of the board?");
+  if (input === null) {
+    // User canceled the prompt
+    return null;
   }
-  createGrid(newSize);
-});
+  input = parseInt(input);
+  if (isNaN(input) || input <= 0 || input > 100) {
+    alert("Please provide a valid number between 1 and 100.");
+    return getSize(); // Retry input
+  } else {
+    alert("Now you can draw!");
+    return input;
+  }
+}
 
-createGrid(16); // Initial grid creation
+function colorDiv(event) {
+  if (click) {
+    const div = event.target;
+    if (color === "random") {
+      div.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+      div.style.backgroundColor = color;
+    }
+  }
+}
+
+function resetBoard() {
+  const divs = document.querySelectorAll(".board div");
+  divs.forEach((div) => (div.style.backgroundColor = "white"));
+}
+
+function changeColor() {
+  const newColor = prompt("Enter a color (e.g., 'red', '#00ff00', 'rgb(255, 0, 0)'):");
+  if (newColor !== null) {
+    color = newColor;
+  }
+}
